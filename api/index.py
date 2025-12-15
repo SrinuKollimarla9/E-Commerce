9e6e24556f24b5ec642cdf9c80dd0a1009bc6c00
SrinuKollimarla9 <2200031821@kluniversity.in>
2025-12-15 10:25:43 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,1 @@
-import os
-from django.core.wsgi import get_wsgi_application
-from vercel_wsgi import make_handler
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "ecommerce_site.settings"
-)
-
-application = get_wsgi_application()
-handler = make_handler(application)
